5f2c6d2cc41a084b1f70af0161ce6ff440c13371
许宏玮 <xuhongweimaggie@126.com>
2022-03-01 22:17:57 +0800

    test

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -3,6 +3,9 @@ package com.example;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
-
     }
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static void test() {}
 }
